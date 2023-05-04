17286bceb08fad935ab37a83e8da2114c7b95310
Alex Kiburu <alexkiburu@gmail.com>
2023-05-04 14:28:44 +0300

    - Add OpenApi Swagger integration - Add springboot starter validation plugin to sort below error:
       jakarta.validation.NoProviderFoundException: Unable to create a Configuration,
       because no Jakarta Bean Validation provider could be found. Add a provider like Hibernate Validator (RI) to your classpath.
    
    - Add OpenApi configuration
    - Add security schemes to enable JWT authentication for all urls except the exempted ones

---
+++ src/main/java/com/kiburu/security/config/OpenApiConfiguration.java
@@ -0,0 +1,54 @@
+package com.kiburu.security.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "AlexKiburu",
+                        email="alexkiburu@gmail.com",
+                        url="https://awesomeapplications.com"
+                ),
+                description = "OpenApi Documentation for Spring Security",
+                title = "OpenApi specification - AlexKiburu",
+                version = "1.0",
+                license = @License(
+                        name = "License Name (MIT)",
+                        url = "https://somelicenseurl.com"
+                ),
+                termsOfService = "Terms of Service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Production ENV",
+                        url = "https://prodUrl.com"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfiguration {
+}
